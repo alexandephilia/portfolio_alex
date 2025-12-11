411e09d289dcd9f8304f77b1e5f88b54ce73d363
0xnihilist <0xnihilist@gmail.com>
2025-12-11 13:16:00 +0700

    Update GrainEffect.tsx

--- components/effects/GrainEffect.tsx
+++ components/effects/GrainEffect.tsx
@@ -21,12 +21,12 @@ const GrainEffect: React.FC = () => {
       </style>
       <div 
         className="fixed inset-0 pointer-events-none z-[2147483647]"
-        style={{ mixBlendMode: 'multiply', opacity: 0.3 }}
+        style={{ mixBlendMode: 'multiply', opacity: 0.8 }}
       >
         <div 
           className="absolute -top-full -left-full w-[300%] h-[300%] will-change-transform"
           style={{
-            backgroundColor: '#0000007b',
+            backgroundColor: '#00000055',
             maskImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.6' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")`,
             WebkitMaskImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.6' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")`,
             maskSize: '150px 150px',
